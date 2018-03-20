b5bb14ba52196a656a2d848c66675561df9ed6ad
Avinash Kumar <avinashkumar3497@gmail.com>
2018-03-20 21:37:38 +0530

    Placed the three spinners in place and done with the string-array of select sem

--- app/src/main/java/com/example/avinashkumar/enolved/DataEntry.java
+++ app/src/main/java/com/example/avinashkumar/enolved/DataEntry.java
@@ -3,6 +3,8 @@ package com.example.avinashkumar.enolved;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
 public class DataEntry extends AppCompatActivity {
 
@@ -10,6 +12,14 @@ public class DataEntry extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_entry);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.sem, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
     @Override
